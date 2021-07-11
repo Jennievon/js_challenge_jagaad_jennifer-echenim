7c875110fe0d7280aa0c1e87d0e53de5b99ef10b
Jennie <echenimjennifr@gmail.com>
2021-07-11 22:09:13 +0100

    setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,8 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -35,7 +34,22 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
+
+// Axios module configuration: https://go.nuxtjs.dev/config-axios
+axios: {
+  credentials: false,
+  baseURL: 'https://api.musement.com/api/v3/',
+  headers: {
+    common: {
+      'Content-Type': 'application/json',
+      'Accept-Language': 'it',
+      'X-Musement-Currency': 'EUR',
+      'X-Musement-Version': '3.4.0'
+    },
+  }
+},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
